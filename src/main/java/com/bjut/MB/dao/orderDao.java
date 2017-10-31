00fac0df52448e8414eb74e77db8f13e37a9598b
[290623829] <[290623829@qq.com]>
2017-10-31 14:20:12 +0800

    9

---
+++ src/main/java/com/bjut/MB/dao/orderDao.java
@@ -0,0 +1,7 @@
+package com.bjut.MB.dao;
+
+/**
+ * Created by Administrator on 2017/10/31.
+ */
+public class orderDao {
+}
---
+++ src/main/java/com/bjut/MB/service/order.java
@@ -0,0 +1,7 @@
+package com.bjut.MB.service;
+
+/**
+ * Created by Administrator on 2017/10/31.
+ */
+public class order {
+}
