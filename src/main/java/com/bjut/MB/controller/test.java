c62284de91b45df457565bb87b826cd273eb6c78
[290623829@qq.com] <[290623829@qq.com]>
2017-10-31 12:57:04 +0800

    第一次提交测试

---
+++ src/main/java/com/bjut/MB/controller/test.java
@@ -0,0 +1,17 @@
+package com.bjut.MB.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2017/10/31.
+ */
+@Controller
+public class test {
+    @RequestMapping(value = "/test")
+    @ResponseBody
+    public String test(){
+        return "HELLO WORLD!!";
+    }
+}
