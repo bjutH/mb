20b6bf541c413f754b8235f9be69b83c39aab9b9
[290623829] <[290623829@qq.com]>
2017-11-03 09:59:56 +0800

    13

--- src/main/java/com/bjut/MB/controller/MemoController.java
+++ src/main/java/com/bjut/MB/controller/MemoController.java
@@ -1,7 +1,12 @@
 package com.bjut.MB.controller;
 
+import com.bjut.MB.service.MemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Administrator on 2017/11/3.
  */
 public class MemoController {
+    private static final Logger logger = LoggerFactory.getLogger(MemoController.class);
 }
