da2df1578c2adf24aadfcad624f73b203c8f7fc8
wwt2017 <624850157@qq.com>
2017-11-06 14:41:28 +0800

    前面的随工单，检查单处理中

---
+++ src/main/java/com/bjut/MB/dao/PackageList.java
@@ -0,0 +1,25 @@
+package com.bjut.MB.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * Created by Cheng on 2017/10/31.
+ */
+public interface PackageList {
+    String TABLE_NAME = "package_record";
+    String INSERT_FIELDS = "product_num, item";
+    String SELECT_FIELDS = "confirm, self_check, packager";
+
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{item}"})
+    void addProcess(String product_num,String item);
+
+    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and item = #{item}"})
+    String selectConfirmAndPackager(String product_num, String item);
+
+    @Update({"update ", TABLE_NAME, "set confirm = #{confirm}, self_check = #{self_check}, packager = #{packager} where product_num = #{product_num} and item = #{item}"})
+    void UpdateConfirmAndPackager(String confirm, String self_check, String packager);
+
+}
---
+++ src/main/java/com/bjut/MB/dao/Yiqi.java
@@ -0,0 +1,24 @@
+package com.bjut.MB.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * Created by Cheng on 2017/10/31.
+ */
+public interface Yiqi {
+    String TABLE_NAME = "yiqi_ps";
+    String INSERT_FIELDS = "product_num, ps_name";
+    String SELECT_FIELDS = "num, pad_id, hanjie, debug, check, soft_num, ps";
+
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{ps_name}"})
+    void addProcess(String orderNum,String ps_name);
+
+    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and process = #{ps_name}"})
+    String selectOperaterAndOhter(String product_num, String ps_name);
+
+    @Update({"update ", TABLE_NAME, "set num = #{num}, pad_id = #{pad_id}, hanjie = #{hanjie} , debug = #{debug}, check = #{check}, soft_num = #{soft_num}, ps = #{ps} where product_num = #{product_num} and ps_name = #{ps_name}"})
+    void UpdateOperaterAndOther(String product_num, String ps_name, String num, String pad_id, String hanjie, String debug, String check, String soft_num, String ps);
+}
