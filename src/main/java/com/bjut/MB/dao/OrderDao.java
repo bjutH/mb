387e4e56fa3df6a7cc55d649974425406ad56696
wwt2017 <624850157@qq.com>
2017-11-06 10:31:52 +0800

    test

--- src/main/java/com/bjut/MB/dao/OrderDao.java
+++ src/main/java/com/bjut/MB/dao/OrderDao.java
@@ -1,11 +1,27 @@
 package com.bjut.MB.dao;
-
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+
 
 /**
  * Created by Administrator on 2017/10/31.
  */
 @Mapper
 public interface OrderDao {
-    public int addOrder(String orderNum,String process);
+    String TABLE_NAME = "suigongdan";
+    String INSERT_FIELDS = "product_num, process";
+    String SELECT_FIELDS = "operater, other, ps";
+
+
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{process}"})
+    void addProcess(String product_num,String process);
+
+    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and process = #{process}"})
+    String selectOperaterAndOhter(String product_num, String process);
+
+    @Update({"update ", TABLE_NAME, "set operater = #{operater}, other = #{other}, ps = #{ps} where product_num = #{product_num} and process = #{process}"})
+    void UpdateOperaterAndOther(String product_num, String process, String other, String ps);
 }
