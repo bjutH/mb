c9bcbecf9cb7a9123ebbd68c05b4d84acfcf9df4
[290623829] <[290623829@qq.com]>
2017-11-06 11:24:04 +0800

    0.5.0

--- src/main/java/com/bjut/MB/service/ProductTestService.java
+++ src/main/java/com/bjut/MB/service/ProductTestService.java
@@ -1,7 +1,10 @@
 package com.bjut.MB.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Administrator on 2017/11/6.
  */
+@Service
 public class ProductTestService {
 }
