af5db9dff6a697d2cf4475911bea1ffb78371e73
wwt2017 <624850157@qq.com>
2017-11-07 12:02:56 +0800

    增加了delete

--- src/main/java/com/bjut/MB/dao/LaoHuaListDao.java
+++ src/main/java/com/bjut/MB/dao/LaoHuaListDao.java
@@ -3,6 +3,7 @@ package com.bjut.MB.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Delete;
 
 /**
  * Created by Cheng on 2017/10/31.
@@ -10,16 +11,18 @@ import org.apache.ibatis.annotations.Update;
 public interface LaoHuaList {
     String TABLE_NAME = "laohua";
     String INSERT_FIELDS = "product_num, demand";
-    String SELECT_FIELDS = "result, process_result, ps, debuger";
+    String SELECT_FIELDS = "*";
 
 
-    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{demand}"})
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{demand})"})
     void addProcess(String product_num,String demand);
 
-    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and demand = #{demand}"})
+    @Select({"select ", SELECT_FIELDS, "from ", TABLE_NAME, "where product_num = #{product_num} and demand = #{demand}"})
     String selectResultAndOther(String product_num, String demand);
 
     @Update({"update ", TABLE_NAME, "set result = #{result}, process_result = #{process_result}, ps = #{ps} debuger = #{debuger} where product_num = #{product_num} and demand = #{demand}"})
     void UpdateResultAndOther(String product_num, String demand, String result, String process_result, String ps, String debuger);
 
+    @Delete({"delete from ", TABLE_NAME, "where product_num = #{product_num} and demand = #{demand}"})
+    void DeleteItem(String product_num, String demand);
 }
--- src/main/java/com/bjut/MB/dao/YiqiDao.java
+++ src/main/java/com/bjut/MB/dao/YiqiDao.java
@@ -3,6 +3,7 @@ package com.bjut.MB.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.Delete;
 
 /**
  * Created by Cheng on 2017/10/31.
@@ -10,15 +11,18 @@ import org.apache.ibatis.annotations.Update;
 public interface Yiqi {
     String TABLE_NAME = "yiqi_ps";
     String INSERT_FIELDS = "product_num, ps_name";
-    String SELECT_FIELDS = "num, pad_id, hanjie, debug, check, soft_num, ps";
+    String SELECT_FIELDS = "*";
 
 
-    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{ps_name}"})
+    @Insert({"insert into ", TABLE_NAME, "(", INSERT_FIELDS, ") values (#{product_num}, #{ps_name})"})
     void addProcess(String orderNum,String ps_name);
 
-    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and process = #{ps_name}"})
+    @Select({"select ", SELECT_FIELDS, "from", TABLE_NAME, "where product_num = #{product_num} and ps_name = #{ps_name}"})
     String selectOperaterAndOhter(String product_num, String ps_name);
 
     @Update({"update ", TABLE_NAME, "set num = #{num}, pad_id = #{pad_id}, hanjie = #{hanjie} , debug = #{debug}, check = #{check}, soft_num = #{soft_num}, ps = #{ps} where product_num = #{product_num} and ps_name = #{ps_name}"})
     void UpdateOperaterAndOther(String product_num, String ps_name, String num, String pad_id, String hanjie, String debug, String check, String soft_num, String ps);
+
+    @Delete({"delete from", TABLE_NAME ,"where product_num = #{product_num}, ps_name = #{ps_name}"})
+    void DeleteItem(String product_name, String ps_name);
 }
