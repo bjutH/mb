64fc273ee45d69f3e6b92aa9f2f99cec3f365593
[290623829] <[290623829@qq.com]>
2017-12-11 17:04:55 +0800

    1.1.2 修改打开随工单不能正确加载插件

--- src/main/java/com/bjut/MB/controller/OrderController.java
+++ src/main/java/com/bjut/MB/controller/OrderController.java
@@ -279,7 +279,6 @@ public class OrderController {
             return "redirect:/homepage/ordermanagement";
         }
         session.setAttribute("orderNum",orderNum);
-        model.addAttribute("search","true");
         return "redirect:/homepage/ordermanagement";
     }
 
